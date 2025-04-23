b10fe7539411c618c64c9b08d0528beb32376bc4
priyankSukhadiya <priyankpatel5529@gmail.com>
2025-04-23 19:16:09 +0530

    Update 10_word_frequency.py

--- LAB_9/10_word_frequency.py
+++ LAB_9/10_word_frequency.py
@@ -4,4 +4,4 @@ def frequency(s):
     freq = {word: words.count(word) for word in set(words)}
     return dict(sorted(freq.items()))
 
-print(frequency("this is a test this is only a test"))
+print(frequency("this is a test this is only a testt"))
